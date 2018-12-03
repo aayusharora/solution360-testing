02ec0585963ee2169a6aaa9e32c84a78fed5272c
aayush arora <aayush113002@gmail.com>
2018-12-03 23:24:05 +0530

    Added requests rate

--- main.go
+++ main.go
@@ -75,8 +75,8 @@ AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36"},"tra
 
 				check(err)
 				fmt.Println(body)
-				rate := vegeta.Rate{Freq: 1000, Per: time.Second}
-				duration := 4 * time.Second/1000
+				rate := vegeta.Rate{Freq: 10000, Per: time.Second}
+				duration := time.Second/10000
 				targeter := vegeta.NewStaticTargeter(vegeta.Target{
 					Method: "POST",
 					URL:    "https://go-test-dot-solution360-event.appspot.com/pubsub/publish?token=token69",
